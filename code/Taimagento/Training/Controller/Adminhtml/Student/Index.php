189e1b11002f6ccb7f1e2bcbd6cc7269a0777a57
bbug <Yuntips1702@gmail.com>
2022-05-19 02:25:48 +0700

    first commit

---
+++ code/Taimagento/Training/Controller/Adminhtml/Student/Index.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace MyCompany\ExampleAdminNewPage\Controller\Adminhtml\Student;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Taimagento_Training::greetings_student';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * 
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Trang admin index'));
+
+        return $resultPage;
+    }
+}
